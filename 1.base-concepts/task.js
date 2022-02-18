2ad7467c58f1105a4364ea193a6927dbd97b81ed
Maria Vinogradova <mari-msk@mail.ru>
2022-02-19 02:16:00 +0800

    task1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,22 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  if (a==0) return alert("Ошибка, а=0 - недопустимое значение!");
+
+  let arr = [];
+  const D = b*b-4*a*c;
+  let x1, x2;
+  arr[0] = x1;
+  arr[1] = x2;
+  if(D>0) {
+    x1 = (-b+Math.sqrt(D))/(2*a);
+    x2 = (-b-Math.sqrt(D))/(2*a); 
+    return arr;
+
+  } else if(D==0) {
+    x1 = -b/(2+a);
+    return arr;
+    
+  } else if(D<0) return "Корней нет"
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
